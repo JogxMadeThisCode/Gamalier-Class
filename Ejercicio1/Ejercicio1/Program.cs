bedb90e178658dc117332297ac4ddecdb5855928
JohnDev <155394271+JogxMadeThisCode@users.noreply.github.com>
2026-05-19 16:02:31 -0400

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calcular2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce 2 numeros para ver los resultados:");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Suma: " + (num1 + num2));
+            Console.WriteLine("Resta: " + (num1 - num2));
+            Console.WriteLine("Multiplicacion: " + (num1 * num2));
+            Console.WriteLine("Division: " + (num1 / num2));
+        }
+    }
+}

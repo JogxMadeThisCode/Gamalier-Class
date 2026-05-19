19a611d866cf30b8ff123fe8e5216e2cffda8610
JohnDev <155394271+JogxMadeThisCode@users.noreply.github.com>
2026-05-19 16:05:28 -0400

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImporteProducto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IMPORTE DE PRODUCTO");
+
+            Console.Write("\nIntroduce el precio del articulo: ");
+            if (precio != int)
+            {
+                Console.WriteLine("El precio debe ser un número válido.");
+                return;
+            }
+            else {
+                float precio = float.Parse(Console.ReadLine());
+
+                Console.Write("Introduce la cantidad: ");
+                float cantidad = float.Parse(Console.ReadLine());
+
+                float importe = precio * cantidad;
+                Console.WriteLine("El importe total es: " + importe);
+            }
+        }
+    }
+}

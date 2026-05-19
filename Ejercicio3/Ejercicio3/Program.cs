ae4cbf15717951b2340e34592659806549c2ad12
JohnDev <155394271+JogxMadeThisCode@users.noreply.github.com>
2026-05-19 16:04:08 -0400

    Add files via upload

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calcular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce 3 numeros para ver los resultados:");
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float num3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Suma: " + (num1 + num2 + num3));
+            Console.WriteLine("Resta: " + (num1 - num2 - num3));
+            Console.WriteLine("Multiplicacion: " + ((num1 * num2) * num3));
+            Console.WriteLine("Division: " + ((num1 / num2) / num3));
+        }
+    }
+}
